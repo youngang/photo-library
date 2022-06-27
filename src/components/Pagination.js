d4c0e1c87a14d7ba1ff78277ca2706e225615d21
youngang <youngang@umich.edu>
2022-06-27 17:05:17 -0400

    working on pagination to link back to app.js, make a new request when the prev or next button is selected

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,21 +1,31 @@
 import React, { Component } from "react";
 
 const Pagination = ({ paginate }) => {
-    const pageNumbers = [];
-  
-    for (let i = 1; i <= 5; i++) {
-      pageNumbers.push(i);
-    }
+  const pageNumbers = [];
+
+  for (let i = 1; i <= 8; i++) {
+    pageNumbers.push(i);
+  }
   
     return (
     <div className="pagination">
         <a><ion-icon name="chevron-back-outline"></ion-icon></a>
         {pageNumbers.map(num => (
-            <a href='!#'>{num}</a>
+          <a onClick={() => paginate(num)} href='!#' className='page-link'>
+            {num}
+          </a>
         ))}
         <a><ion-icon name="chevron-forward-outline"></ion-icon></a>
     </div>
     );
   };
+
+  const handlePrevPage = (() => {
+    console.log('hi');
+  });
+
+  const handleNextPage = (() => {
+    console.log('hi');
+  });
   
   export default Pagination;
